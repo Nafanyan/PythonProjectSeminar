a78b81eb6043b830635fa7005d1d28b7dac4ac6f
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-01-31 09:30:53 +0300

    fix 13 task

--- seminar_3_4/Task13.py
+++ seminar_3_4/Task13.py
@@ -1,15 +1,6 @@
 #Пользователь задаёт две строки. Определить количество количество вхождений одной строки в другой.
 def peresech(one_str,two_str):
-    if (one_str in two_str):
-        count = 0
-        setting = ''
-        for i in range(len(two_str)-len(one_str)+1):
-            for j in range(len(one_str)):
-                setting = setting + two_str[i+j]
-            if (setting == one_str): count += 1
-            setting = ''
-        return count
-    else: return False
+    return (two_str.count(one_str))
 
 one_string = input()
 two_string = input()
