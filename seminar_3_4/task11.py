c8e02d8888954a94b90ed5dd614db66e80fa83c6
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-01 22:02:58 +0300

    убрал лишнее в task11

--- seminar_3_4/task11.py
+++ seminar_3_4/task11.py
@@ -1,4 +1,3 @@
 #Для натурального N создать множество: 1, -3, 9, -27, 81 и т.д.
 N = 5
-arr = list(map(lambda arr: -3 **arr, [x for x in range(N)]))
-print(arr)
+print(list(map(lambda arr: -3 **arr, [x for x in range(N)])))
