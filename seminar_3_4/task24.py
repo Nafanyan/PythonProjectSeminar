ffe86435de424cb9a83896646d56c2e820054af2
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-02 18:19:52 +0300

    fix task 24

--- seminar_3_4/task24.py
+++ seminar_3_4/task24.py
@@ -1,11 +1,7 @@
 #В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением дробной части элементов.
 # Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-def dif_max_min(arr):
-    sup = []
-    for el in arr:
-        sup.append(round(el - int(el),3))
-    return max(sup) - min(sup)
-
 array = [1.1, 1.2, 3.1, 5.2, 10.01]
-print(dif_max_min(array))
+arr = list(map(lambda arr: round(arr - int(arr),3),array))
+result = max(arr) - min(arr)
+print(result)
