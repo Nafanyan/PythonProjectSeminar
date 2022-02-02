5e00c891845a2ebd4d96edf88da897dd9f55df7f
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-02 17:12:56 +0300

    проагрейдил task12

--- seminar_3_4/task12.py
+++ seminar_3_4/task12.py
@@ -1,9 +1,4 @@
 #Для натурального N создать словарь индекс-значение, состоящий из элементов последовательности 3k + 1.
-def dictionary_create(N):
-    res_dic = {}
-    for i in range(N):
-        res_dic[i] = 3 * i + 1
-    return res_dic
-
-print(dictionary_create(6))
-
+N = 6
+dic = {x: 3 * x + 1 for x in range (N)}
+print(dic)
