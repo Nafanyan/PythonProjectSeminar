fe2d3389d171f02e74090d59feb2af8b75339896
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-02 18:25:19 +0300

    fix task 27

--- seminar_3_4/task27.py
+++ seminar_3_4/task27.py
@@ -1,10 +1,5 @@
 #Строка содержит набор чисел. Показать большее и меньшее число
-def min_max(text):
-    text = text.split()
-    sup_arr = []
-    for el in text:
-        sup_arr.append(int(el))
-    return max(sup_arr),min(sup_arr)
 
 numbers = '123 24 4212 62'
-print(min_max(numbers))
+num = list(map(int,numbers.split()))
+print(max(num), min(num))
