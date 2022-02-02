b4825de309ffe9e0bf4c30a5f1b46cd8b3322cb5
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-02 17:19:01 +0300

    проапгрейдил task16

--- seminar_3_4/task16.py
+++ seminar_3_4/task16.py
@@ -1,9 +1,4 @@
 #Задать список из n чисел последовательности (1+1n)n и вывести на экран их сумму
-
-def sum_sequence(n):
-    result = 0
-    for i in range(1, n+1):
-        result = result + (1+1/i) ** i
-    return result
-
-print(sum_sequence(4))
+N = 4
+arr = [(1* 1 * x)*x for x in range(N)]
+print(sum(arr))
