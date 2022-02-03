1ccbe57a55206125398a65bbac0a27316a0fa766
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-03 19:34:48 +0300

    исправил task38

--- seminar_5_6/task38.py
+++ seminar_5_6/task38.py
@@ -1,5 +1,9 @@
 #Напишите программу, удаляющую из текста все слова содержащие "абв".
 text_delet = 'как'
-text = 'так вот тут должен быть текст, никак иначе, как что-то умное,как-то наверное...'
-text = text.replace(text_delet,'')
-print(text)
+text_S = 'так вот тут должен быть текст, никак иначе, как что-то умное, как-то наверное...'
+
+text_S = text_S.split()
+text_S = list(filter(lambda el: el.find(text_delet) == -1, text_S))
+result = " ".join(text_S)
+print(result)
+
